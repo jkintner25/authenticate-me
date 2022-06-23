83c7779573d924b9838ef028bd2bfc1771bcef5a
jkintner25 <jkintner25@gmail.com>
2022-06-23 15:22:45 -0500

    initial frontend setup

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 
 router.get(
     '/',
@@ -19,8 +31,10 @@ router.get(
     }
 );
 
+// Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
         const { credential, password } = req.body;
 
